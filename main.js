d4a5d289d3ef7ce35d18f1c8b25565c2e87f0689
madrndx <rndx.ra@gmail.com>
2023-07-11 16:47:37 +0800

    update

--- main.js
+++ main.js
@@ -1,3 +1,2 @@
-AOS.init();
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
