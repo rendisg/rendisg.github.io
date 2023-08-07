5680c474c8c87710b98d3fe7ed722c4cd0542f8e
rendi <rendi.abdul@germs.sg>
2023-08-07 13:25:49 +0700

    asw

--- js/animasi.js
+++ js/animasi.js
@@ -1,3 +1,12 @@
 AOS.init();
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+var navBar= document.getElementById('navBar')
+window.onscroll = function (){
+    if(window.scrollY > 22 ){
+        navBar.classList.add('scrolled');
+    }else{
+        navBar.classList.remove('scrolled');
+    }
+};
